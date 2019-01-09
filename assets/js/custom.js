513d5624be6e55f05424d5eda3259a8964dc73cd
Nazifa Rashid <nazifarashid1822@gmail.com>
2019-01-09 18:18:06 +0600

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    //typed JS
+    var typed = new Typed('.typed', {
+        stringsElement: '#typed-strings',
+        typeSpeed: 40,
+        backSpeed: 25,
+        loop: true
+      });
+
+    /*var typed = new Typed(".typed", {
+      // Waits 1000ms after typing "First"
+      strings: ["First ^1000 sentence.", "Second sentence."]
+    });*/
+
+
+});
+
+$(document).ready(function(){
+$(".owl-carousel").owlCarousel();
+});
